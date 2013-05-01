f5353c7c0bb6199ba28e148a082db913a22f8dae
Alexander Bubeck <mail@cybernotic.org>
2013-05-01 18:19:04 +0200

    initial version

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# mostly adapted from https://github.com/holman/dotfiles
+
+
+link_files () {
+	for source in `find ~/.dotfiles -maxdepth 2 -name \*.symlink`
+  	do
+  		dest="$HOME/.`basename \"${source%.*}\"`"
+
+	    if [ -f $dest ] || [ -d $dest ]
+	    then
+	    	printf "Backup of existing file \n"
+	    	mv $dest $dest\.backup
+	    fi
+	    ln -s $source $dest
+      printf "Installed $dest \n"
+	done
+}
+
+link_files
